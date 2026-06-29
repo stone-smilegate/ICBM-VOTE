89dfbd0394a4e41799e368ce7c9845990974398f
stone-smilegate <wseoks@smilegate.com>
2026-06-29 15:30:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const GITHUB_TOKEN  = "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";                  // PAT 붙여넣기 (Issues R/W)
+const GITHUB_TOKEN  = "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";                  // PAT 붙여넣기 (Issues R/W)
 const REPO_OWNER    = "stone-smilegate";
 const REPO_NAME     = "ICBM-VOTE";
 const ISSUE_NUMBER  = "1";
