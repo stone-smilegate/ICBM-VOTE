5c699275814f4a97464737c374f11eb61d367714
stone-smilegate <wseoks@smilegate.com>
2026-06-29 15:23:43 +0900

    Update GITHUB_TOKEN in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const GITHUB_TOKEN  = "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";                  // PAT 붙여넣기 (Issues R/W)
+const GITHUB_TOKEN  = "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";                  // PAT 붙여넣기 (Issues R/W)
 const REPO_OWNER    = "stone-smilegate";
 const REPO_NAME     = "ICBM-VOTE";
 const ISSUE_NUMBER  = "1";
