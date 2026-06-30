39da843af82918cdde40b0d35cf5a0a567ae0ef6
stone-smilegate <wseoks@smilegate.com>
2026-06-30 14:19:52 +0900

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const GITHUB_TOKEN  = "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";                  // PAT 붙여넣기 (Issues R/W)
-const REPO_OWNER    = "stone-smilegate";
-const REPO_NAME     = "ICBM-VOTE";
-const ISSUE_NUMBER  = "1";
- 
